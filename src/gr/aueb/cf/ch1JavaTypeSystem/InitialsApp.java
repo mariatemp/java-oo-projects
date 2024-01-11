142c181a2c9dbaba1fe189f58594e92b788246a3
Maria Temponera <marakitempo@yahoo.gr>
2024-01-11 12:10:28 +0200

    Main class initial

---
+++ src/gr/aueb/cf/ch1JavaTypeSystem/InitialsApp.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.ch1JavaTypeSystem;
+
+/**
+ * Εκτυπώνει στην κονσόλα (std output)
+ * τα αρχικά με αστεράκια.
+ */
+public class InitialsApp {
+    public static void main(String[] args) {
+        System.out.println("*     *   ******");
+        System.out.println("*  *  *      *");
+        System.out.println("*  *  *      *");
+        System.out.println("*     *      *");
+
+    }
+}
---
+++ src/gr/aueb/cf/ch1JavaTypeSystem/SumResult.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch1JavaTypeSystem;
+
+public class SumResult {
+    public static void main(String[] args) {
+        //Δήλωση και αρχικοποίηση μεταβλητών
+
+        int num1 = 19;
+        int num2 = 30;
+        int result = 0;
+
+        //Εντολές
+        result = num1 + num2;
+
+        //Εκτύπωση αποτελέσματος
+        System.out.println("Το αποτέλεσμα είναι:" + result);
+    }
+}
